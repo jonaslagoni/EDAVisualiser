a9b18aeabd8fdf106fe0c073df05cf93bda6291e
Matatjahu <urbanczyk.maciej.95@gmail.com>
2022-05-24 16:36:10 +0200

    fix rebase conflicts

--- examples/simple-react/src/simpleSystem.tsx
+++ examples/simple-react/src/simpleSystem.tsx
@@ -10,77 +10,92 @@ import './simple.css';
 function App() {
   return (
     <div className="App">
-      <SystemView>
-        <Application
-          id="string"
-          defaultContentType="string"
-          description="string"
-          title="string"
-          version="string"
-          license={{
-            name: 'string',
-            url: 'string',
-          }}
-          externalDocs="string"
-          servers={[
-            {
-              name: 'string',
-              url: 'string',
-              description: 'string',
-              protocol: 'string',
-              protocolVersion: 'string',
+      <SystemView
+        applications={[
+          {
+            application: {
+              id: "string",
+              defaultContentType: "string",
+              description: "string",
+              title: "string",
+              version: "string",
+              license: {
+                name: 'string',
+                url: 'string',
+              },
+              externalDocs: "string",
+              servers: [
+                {
+                  name: 'string',
+                  url: 'string',
+                  description: 'string',
+                  protocol: 'string',
+                  protocolVersion: 'string',
+                },
+              ],
             },
-          ]}
-        >
-          <Outgoing
-            channel="Test/test/test"
-            description="Test description"
-            id="testid2"
-            messages={[{ title: 'test' }]}
-          />
-          <Incoming
-            channel="Test/test/"
-            description="Test description"
-            id="testid"
-            messages={[{ title: 'test' }]}
-          />
-        </Application>
-
-        <Application
-          id="string2"
-          defaultContentType="string"
-          description="string"
-          title="string"
-          version="string"
-          license={{
-            name: 'string',
-            url: 'string',
-          }}
-          externalDocs="string"
-          servers={[
-            {
-              name: 'string',
-              url: 'string',
-              description: 'string',
-              protocol: 'string',
-              protocolVersion: 'string',
+            incomingOperations: [
+              {
+                channel: "Test/test/",
+                description: "Test description",
+                id: "testid",
+                messages: [{ title: 'test' }],
+                forApplication: 'string',
+              }
+            ],
+            outgoingOperations: [
+              {
+                channel: "Test/test/test",
+                description: "Test description",
+                id: "testid2",
+                messages: [{ title: 'test' }],
+                forApplication: 'string',
+              }
+            ],
+          },
+          {
+            application: {
+              id: "string2",
+              defaultContentType: "string",
+              description: "string",
+              title: "string",
+              version: "string",
+              license: {
+                name: 'string',
+                url: 'string',
+              },
+              externalDocs: "string",
+              servers: [
+                {
+                  name: 'string',
+                  url: 'string',
+                  description: 'string',
+                  protocol: 'string',
+                  protocolVersion: 'string',
+                },
+              ],
             },
-          ]}
-        >
-          <Incoming
-            channel="Test/test/test"
-            description="Test description"
-            id="testid2"
-            messages={[{ title: 'test' }]}
-          />
-          <Outgoing
-            channel="Test/test/"
-            description="Test description"
-            id="testid"
-            messages={[{ title: 'test' }]}
-          />
-        </Application>
-      </SystemView>
+            outgoingOperations: [
+              {
+                channel: "Test/test/",
+                description: "Test description",
+                id: "testid",
+                messages: [{ title: 'test' }],
+                forApplication: 'string2',
+              }
+            ],
+            incomingOperations: [
+              {
+                channel: "Test/test/test",
+                description: "Test description",
+                id: "testid2",
+                messages: [{ title: 'test' }],
+                forApplication: 'string2',
+              }
+            ],
+          }
+        ]}
+      />
     </div>
   );
 }
